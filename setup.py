d75ecf85e7e4ee9c1e2acbbaca972d914daf8138
ravsemirnov <vsemirnov.roman@gmail.com>
2023-10-20 11:17:28 +0200

    Setup project files and directories

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a tool for expansion of images in browser',
+    author=': Vsemirnov R. and Dukin N.',
+    license='MIT',
+)
