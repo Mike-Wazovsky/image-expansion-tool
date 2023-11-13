35be48cd2390ad6c6acab16a896faf1247a242a7
Nikita Dukin <kir.dukin@gmail.com>
2023-11-13 21:16:35 +0100

    fix(*): add downloading data

--- src/data/download_data.sh
+++ src/data/download_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mOEP6woH_9Ox8_Va0V5DibWttYR6u930' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mOEP6woH_9Ox8_Va0V5DibWttYR6u930" -O data/raw/train_dataset.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mOEP6woH_9Ox8_Va0V5DibWttYR6u930' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mOEP6woH_9Ox8_Va0V5DibWttYR6u930" -O data/raw/dataset.zip && rm -rf /tmp/cookies.txt
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,7 +12,7 @@ filename = "dataset.zip"
 def download_data():
     subprocess.run(["chmod", "+x", "./src/data/download_data.sh"])
     subprocess.run(["./src/data/download_data.sh"])
-    subprocess.run(["tar", "cvfz", "./data/raw/train_dataset.zip", "./data/processed/"])
+    subprocess.run(["unzip", "./data/raw/dataset.zip", "./data/processed/dataset"])
 
 
 @click.command()
