22405849a70b9eeb96e0abeec5669548c03a203e
Nikita Dukin <kir.dukin@gmail.com>
2023-11-13 21:02:11 +0100

    fix(*): add downloading data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -10,8 +10,7 @@ filename = "dataset.zip"
 
 
 def download_data():
-    subprocess.run(
-        """wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mOEP6woH_9Ox8_Va0V5DibWttYR6u930' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mOEP6woH_9Ox8_Va0V5DibWttYR6u930" -O train_dataset.zip && rm -rf /tmp/cookies.txt""")
+    subprocess.run(["./download_data.sh"])
 
 
 @click.command()
