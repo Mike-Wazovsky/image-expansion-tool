cef26cfd2b056ac937617213b9e720ba5c81e1f1
Nikita Dukin <kir.dukin@gmail.com>
2023-11-13 21:49:36 +0100

    fix(*): implement infrastructure

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from src.models.train_model import train_model
 
 batch_size = 32
 
+
 def main():
     train_loader, valid_loader, test_loader = get_dataset(batch_size)
     train_model(train_loader)
+
+
+if __name__ == "__main__":
+    main()
