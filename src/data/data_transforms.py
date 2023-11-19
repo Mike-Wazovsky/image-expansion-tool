527e5d6423c9a832c7a907056a5ef13e0dac2624
Nikita Dukin <kir.dukin@gmail.com>
2023-11-19 01:40:55 +0100

    fix(*): update augmentations

--- src/data/data_transforms.py
+++ src/data/data_transforms.py
@@ -4,6 +4,5 @@ data_transforms = transforms.Compose([
     transforms.RandomResizedCrop(512),
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(15),
-    transforms.ColorJitter(brightness=0.05, contrast=0.05, saturation=0.05, hue=0.05),
     transforms.ToTensor(),
 ])
