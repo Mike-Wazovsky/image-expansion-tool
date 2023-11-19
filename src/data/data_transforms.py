ab94878852ba71d85ca79dc9e1231b2563f8c594
Nikita Dukin <kir.dukin@gmail.com>
2023-11-19 19:00:02 +0100

    check(*): just check what is wrong with model_v2

--- src/data/data_transforms.py
+++ src/data/data_transforms.py
@@ -1,8 +1,8 @@
 from torchvision import transforms
 
 data_transforms = transforms.Compose([
-    transforms.RandomResizedCrop(512),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(15),
+    # transforms.RandomResizedCrop(512),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.RandomRotation(15),
     transforms.ToTensor(),
 ])
