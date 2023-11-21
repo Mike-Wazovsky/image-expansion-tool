fdad7071ab82a37f36ebe1b5e37aeda83aa18e61
Nikita Dukin <kir.dukin@gmail.com>
2023-11-21 17:41:23 +0100

    fix(*): fix

--- src/utils.py
+++ src/utils.py
@@ -10,9 +10,9 @@ def init_logger(cfg: DictConfig, login_id, experiment_notes=None):
     subprocess.run(["wandb", "login", str(login_id)])
     wandb.init(project="image-expansion-tool",
                name="Exp {}".format(str(int(datetime.utcnow().timestamp()))[2:]),
-               notes=experiment_notes if experiment_notes is not None else "",
+               notes=experiment_notes if experiment_notes is not None else str(cfg),
                tags=[cfg.model.version, cfg.dataset.version, cfg.learning.loss, cfg.learning.optimizer_type],
-               config=cfg)
+               config={"str": str(cfg)})
 
 
 def finish_logger():
