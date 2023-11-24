e9afe1d25e2a1ad2fe9f4859ddcf22f4baec8725
Nikita Dukin <kir.dukin@gmail.com>
2023-11-24 02:19:15 +0100

    fix(*): add random crop 512

--- src/data/data_transforms.py
+++ src/data/data_transforms.py
@@ -1,7 +1,7 @@
 from torchvision import transforms
 
 data_transforms = transforms.Compose([
-    # transforms.RandomResizedCrop(512),
+    transforms.RandomResizedCrop(512),
     # transforms.RandomHorizontalFlip(),
     # transforms.RandomRotation(15),
     transforms.ToTensor(),
